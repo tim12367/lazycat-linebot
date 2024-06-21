31fb5878dd49c0905cce1f7e299fbb29441b92af
tim12367 <tim12367@ymail.com>
2024-06-21 22:45:20 +0800

    init project

---
+++ src/main/java/tw/lazycat/lazycat_linebot/LazycatLinebotApplication.java
@@ -0,0 +1,13 @@
+package tw.lazycat.lazycat_linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazycatLinebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazycatLinebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/lazycat/lazycat_linebot/LazycatLinebotApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.lazycat.lazycat_linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazycatLinebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
