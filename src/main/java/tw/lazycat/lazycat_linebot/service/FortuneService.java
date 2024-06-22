40d55482412ec54fb3b0e9a42da89b6bfcd44392
tim12367 <tim12367@ymail.com>
2024-06-22 22:50:35 +0800

    add FortuneService: 抽籤相關模組

---
+++ src/main/java/tw/lazycat/lazycat_linebot/service/FortuneService.java
@@ -0,0 +1,58 @@
+package tw.lazycat.lazycat_linebot.service;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.linecorp.bot.webhook.model.MessageEvent;
+
+/**
+ * 抽籤服務
+ * 
+ * @since 2024-06-22
+ * @author Tim
+ */
+@Service
+@Validated
+public class FortuneService {
+	
+	@Autowired
+	private LineReplyService lineReplyService;
+	
+	public void getFortune(@NotNull MessageEvent event) {
+
+		String reply = getFortuneString();
+
+		lineReplyService.replyText(event.replyToken(), reply);
+	}
+
+	/**
+	 * 隨機抽取籤詩
+	 * 
+	 * @return
+	 */
+	private String getFortuneString() {
+		// 1~100
+		int randomInt = (int) (Math.random() * 100) + 1;
+		return getFortuneString(randomInt);
+	}
+
+	/**
+	 * 輸入數字，拿到對應籤詩
+	 * 
+	 * @param randomInt
+	 * @return
+	 */
+	private String getFortuneString(int randomInt) {
+		if (randomInt <= 30) {
+			return "凶";
+		} else if (randomInt <= 65) {
+			return "吉";
+		} else if (randomInt <= 82) {
+			return "大吉";
+		} else {
+			return "性奴隸\r\nこの結果がでるなんて、ある意味運いいかもね？";
+		}
+	}
+}
